a1b66caa36583de016a48e8b0ed757bdcb8fff40
Shirahata <asuka.meiji@gmail.com>
2021-11-16 11:43:20 +0900

    Revert "Chapter 03"
    This reverts commit 86a1b93f5d1ec1d0817e72b06c3b2916005f7cbe.

--- Assets/Scripts/onlin/SampleScene.cs
+++ Assets/Scripts/onlin/SampleScene.cs
@@ -10,20 +10,8 @@ public class SampleScene : MonoBehaviourPunCallbacks
     // Start is called before the first frame update
     void Start()
     {
-        //プレイヤー自身の名前を"Player"に設定する
-        PhotonNetwork.NickName = "Player";
-
         // PhotonServerSettingsの設定内容を使ってマスターサーバーへ接続する
         PhotonNetwork.ConnectUsingSettings();
-
-        // 自身が管理者かどうかを判定する
-        if (photonView.IsMine)
-        {
-            // 所有者を取得する
-            Player owner = photonView.Owner;
-            // 所有者のプレイヤー名とIDをコンソールに出力する
-            Debug.Log($"{owner.NickName}({photonView.OwnerActorNr})");
-        }
     }
 
     // マスターサーバーへの接続が成功した時に呼ばれるコールバック
@@ -41,19 +29,4 @@ public class SampleScene : MonoBehaviourPunCallbacks
         PhotonNetwork.Instantiate("stage", position, Quaternion.identity);
         PhotonNetwork.Instantiate("Zombieonline", position, Quaternion.identity);
     }
-
-
-    // 他プレイヤーがルームへ参加した時に呼ばれるコールバック
-    public override void OnPlayerEnteredRoom(Player newPlayer)
-    {
-        Debug.Log($"{newPlayer.NickName}が参加しました");
-    }
-
-    // 他プレイヤーがルームから退出した時に呼ばれるコールバック
-    public override void OnPlayerLeftRoom(Player otherPlayer)
-    {
-        Debug.Log($"{otherPlayer.NickName}が退出しました");
-    }
-
-
 }
